13351f48214d67ad43465f7a878cd19a36ac5e6e
Victor Santos <victor.eas19@gmail.com>
2022-07-10 21:28:12 -0300

    Project Finished

--- server.c
+++ server.c
@@ -9,29 +9,49 @@
 #define PORT 8080
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket; long valread;
+    int server_fd, new_socket;
+    long valread;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
+
+    FILE *html_data;
+    html_data = fopen("index.html", "r");
+
+    char response_data[1024];
+    fgets(response_data, 1024, html_data);
+
+    //char http_header[2048] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n";
+    char hello[] =
+    
+    "HTTP/1.1 200 Ok\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+    "<!DOCTYPE html>\r\n"
+    /*"<html><head><title>Sistemas Operacionais</title>\r\n"
+    "<body>Hello World</body>"
+    "</html>";*/
+    "<html><head><title>Sistemas Operacionais</title>\r\n"
+    "<style>body{background-color: light-grey; text-align:center; background-color: lightgrey;font-family: 'Courier New';line-height: 30px;}</style>"
+    "<body><h1>Hello World</h1><h2>Sistemas Operacionais 2022.1</h2><ul><li>Victor Emanuel Alves dos Santos</li><li>John Vasconcelos dos Santos</li><li>Teodoro Raulino Lima Neto</li><li>João Victor Leandro Nunes</li></ul></body>"
+    "</html>";
     
     // Only this line has been changed. Everything is same.
-    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-    
+    //char *hello = "HTTP/1.1 200 OK\nContent-Type: text/html\n\nHello world!";
+    //strcat(http_header, response_data);
+
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("In socket");
         exit(EXIT_FAILURE);
     }
-    
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    
+    address.sin_port = htons(PORT);
+
     memset(address.sin_zero, '\0', sizeof address.sin_zero);
-    
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         perror("In bind");
         exit(EXIT_FAILURE);
@@ -41,19 +61,19 @@ int main(int argc, char const *argv[])
         perror("In listen");
         exit(EXIT_FAILURE);
     }
-    while(1)
+    while (1)
     {
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
         {
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-        
+
         char buffer[30000] = {0};
-        valread = read( new_socket , buffer, 30000);
-        printf("%s\n",buffer );
-        write(new_socket , hello , strlen(hello));
+        valread = read(new_socket, buffer, 30000);
+        printf("%s\n", buffer);
+        write(new_socket, hello, strlen(hello));
         printf("------------------Hello message sent-------------------");
         close(new_socket);
     }
